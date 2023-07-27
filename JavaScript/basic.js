517565dd2cfe05fa95c1feecfeea220ac47dc3b6
professorabhay <abhayetwup123@gmail.com>
2023-07-27 10:29:10 +0530

    Day02 [27-07-2023]

--- JavaScript/basic.js
+++ JavaScript/basic.js
@@ -39,46 +39,46 @@ console.log("Hello World")
 
 
 
-const id= Symbol('123')
-console.log(id) 
-const bigNumber = 223322322228887n
-console.log(typeof bigNumber)
-let name = "John" // string type variable declaration with value John assigned in it
-let age = 25 // number type variable declaration with value 25 assigned in it
-let isApproved = true // boolean type variable declaration with value true assigned in it
-let firstName = undefined // undefined type variable declaration with value undefined assigned in it
-let lastName = null // null type variable declaration with value null assigned in it
-const PI = Math.PI;
-const gravity = 9.8;
+// const id= Symbol('123')
+// console.log(id) 
+// const bigNumber = 223322322228887n
+// console.log(typeof bigNumber)
+// let name = "John" // string type variable declaration with value John assigned in it
+// let age = 25 // number type variable declaration with value 25 assigned in it
+// let isApproved = true // boolean type variable declaration with value true assigned in it
+// let firstName = undefined // undefined type variable declaration with value undefined assigned in it
+// let lastName = null // null type variable declaration with value null assigned in it
+// const PI = Math.PI;
+// const gravity = 9.8;
 
-console.log(name)
-console.log(age)
-console.table([name, age, isApproved, firstName, lastName, PI, gravity])
+// console.log(name)
+// console.log(age)
+// console.table([name, age, isApproved, firstName, lastName, PI, gravity])
 
-console.log(typeof name)
-console.log(typeof age)
-console.log(typeof isApproved)
-console.log(typeof firstName)
-console.log(typeof lastName)
-console.log(typeof PI)
-console.log(typeof gravity)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isApproved)
+// console.log(typeof firstName)
+// console.log(typeof lastName)
+// console.log(typeof PI)
+// console.log(typeof gravity)
 
 
 // Type Conversion ->
 // 1. Implicit Type Conversion
 // 2. Explicit Type Conversion
-let x = '7'    // String to Number conversion using unary plus operator (+x),
-let y = 7      // Number to String conversion using String() function
-console.log(x+y)
-console.log(typeof x)
-console.log(typeof y)   
-console.log(typeof (x+y)) 
-let z = Number(x)
-console.log(typeof z)
+// let x = '7'    // String to Number conversion using unary plus operator (+x),
+// let y = 7      // Number to String conversion using String() function
+// console.log(x+y)
+// console.log(typeof x)
+// console.log(typeof y)   
+// console.log(typeof (x+y)) 
+// let z = Number(x)
+// console.log(typeof z)
 
 
 // Arrays and Objects
-console.log({name, age, isApproved, firstName, lastName, PI, gravity})
+// console.log({name, age, isApproved, firstName, lastName, PI, gravity})
 
 
 
@@ -89,36 +89,36 @@ console.log({name, age, isApproved, firstName, lastName, PI, gravity})
 // 4. Logical Operators -> &&, ||, !
 // 5. Bitwise Operators -> &, |, ^, ~, <<, >>
 
-let num1 = 10
-let num2 = 20
-num1++   // incrementing the first operand before performing addition operation (Pre Increment Operator)
-num2--   // decrementing the second operand before performing subtraction operation (Pre Decrement Operator)
-console.log(num1 + num2)
+// let num1 = 10
+// let num2 = 20
+// num1++   // incrementing the first operand before performing addition operation (Pre Increment Operator)
+// num2--   // decrementing the second operand before performing subtraction operation (Pre Decrement Operator)
+// console.log(num1 + num2)
 
-var num3 = 10
-num3 = 11
-var num3 = 4
+// var num3 = 10
+// num3 = 11
+// var num3 = 4
 
-console.log(num3)
-console.log(49*49)
+// console.log(num3)
+// console.log(49*49)
 
 
 // Important Conversion
 
-let str1 ="hello"
-let str2 ="abhay"
-console.log(str1+str2)
+// let str1 ="hello"
+// let str2 ="abhay"
+// console.log(str1+str2)
 
-console.log(10 + "20") // 1020
-console.log(10 - "20") // -10
-console.log(10 + 20 + "30") // 3030
-console.log("1" + 2 + 2) //122
-console.log(3+4*5%3)
-console.log(+true);
+// console.log(10 + "20") // 1020
+// console.log(10 - "20") // -10
+// console.log(10 + 20 + "30") // 3030
+// console.log("1" + 2 + 2) //122
+// console.log(3+4*5%3)
+// console.log(+true);
 
-const myFunc = function(){
-    console.log("Hello World");
-}
+// const myFunc = function(){
+//     console.log("Hello World");
+// }
 
 
 // Memory 
@@ -131,38 +131,227 @@ const myFunc = function(){
 // 1. Object
 // 2. Array
 // 3. Function
-class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-}
-var personObj= new Person('John', 'Doe');
-console.log(personObj);
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+// var personObj= new Person('John', 'Doe');
+// console.log(personObj);
 
-function addNumbers() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum = sum + arguments[i]
-    }
-    console.log(sum)
-}
-addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+// function addNumbers() {
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum = sum + arguments[i]
+//     }
+//     console.log(sum)
+// }
+// addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
 // Stack Example // always get copy 
-let myName = "Abhay"
-let anotherName = myName
-anotherName = "Arjun"
+// let myName = "Abhay"
+// let anotherName = myName
+// anotherName = "Arjun"
 
-console.log(myName)
-console.log(anotherName)
+// console.log(myName)
+// console.log(anotherName)
 
 // Heap Example // always get reference
-let myArray = [1, 2, 3, 4, 5]
-let anotherArray = myArray
-anotherArray.push(6)
-console.log(myArray)
-console.log(anotherArray)
+// let myArray = [1, 2, 3, 4, 5]
+// let anotherArray = myArray
+// anotherArray.push(6)
+// console.log(myArray)
+// console.log(anotherArray)
 
 
+//DAY-02 (27-07-2023)
+
+// Strings -> 
+
+// Basic String -> 'Abhay'  "Abhay" Can written in " ",  ' '
+
+// 1. String Concatenation -> "Abhay" + "Abhay"
+// modern syntax -> 
+
+// const a = "Abhay"
+// const b = 51
+
+// console.log(a + b)
+// console.log(`hello my name is ${a} and my repo size is ${b}`); // modern way
+// console.log(a[0])
+
+// 2. String Length
+// console.log(a.length)
+
+// 3. String Methods
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+// console.log(a.substring(0, 3))
+// console.log(a.split(''))
+// console.log(a.split('a'))
+
+// 4. String Template Literals
+// console.log(`My name is ${a} and my age is ${b}`)
+// console.log("My name is " + a + " and my age is " + b)
+
+
+// console.log(a.charAt(2));
+// console.log(a.indexOf('y'));
+// const anotherA = a.slice(-4,4)
+// console.log(anotherA)
+// console.log(a.includes('h'))
+// console.log(a.includes('z'))
+// console.log(a.endsWith('y'))
+
+// 5. String Escape Sequence
+// console.log('My name is \'Abhay\'')
+// console.log("My name is \"Abhay\"")
+// console.log("My name is \nAbhay")
+// console.log("My name is \tAbhay")
+// console.log("My name is \\Abhay")
+
+// 6. String Comparison
+// console.log('a' > 'b')
+// console.log('a' < 'b')
+// console.log('a' == 'b')
+// console.log('a' === 'b')
+// console.log('a' != 'b')
+// console.log('a' !== 'b')
+
+// 7. String Searching
+// console.log(a.indexOf('h'))
+// console.log(a.lastIndexOf('h'))
+// console.log(a.search('h'))
+
+// 8. String Extracting
+// console.log(a.slice(0, 3))
+// console.log(a.slice(-4, 4))
+// console.log(a.substring(0, 3))
+// console.log(a.substr(0, 3))
+
+// 9. String Replacing
+// console.log(a.replace('h', 'z'))
+// console.log(a.replaceAll('h', 'z'))
+
+// 10. String Padding
+// console.log(a.padStart(10, '0'))
+// console.log(a.padEnd(10, '0'))
+
+// 11. String Trim
+// console.log('   Abhay   '.trim())
+// console.log('   Abhay   '.trimStart())
+// console.log('   Abhay   '.trimEnd())
+
+// // 12. String Extracting
+// console.log(a.charAt(2))
+// console.log(a.charCodeAt(2))
+// console.log(a[2])
+
+// 13. String Conversion
+// console.log(String(123))
+// console.log(String(123 + 123))
+// console.log(String(123) + String(123))
+
+// 14. String Iteration
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i])
+// }
+
+
+// Number and Maths 
+
+// const balance = new Number(100000)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+// console.log(balance.toString().length);
+// console.log(balance.toLocaleString('en-IN'));
+
+
+// Math Object
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.round(2.4));
+// console.log(Math.ceil(2.4));
+// console.log(Math.floor(2.4));
+// console.log(Math.sqrt(2.4));
+// console.log(Math.abs(-2.4));
+// console.log(Math.pow(2, 4));
+// console.log(Math.min(2, 4, 5, 6, 7, 8, 9, 10));
+// console.log(Math.max(2, 4, 5, 6, 7, 8, 9, 10));
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor((Math.random()*10) + 1));
+// const min = 1
+// const max = 4
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+// Date and Time
+const dateObj = new Date(); // current time stamp object creation using the constructor function of Date class
+// console.log(dateObj);
+// console.log(dateObj.toString());
+// console.log(dateObj.toDateString());
+// console.log(dateObj.toTimeString());
+// console.log(dateObj.toLocaleString());
+// console.log(dateObj.toLocaleDateString());
+// console.log(dateObj.toLocaleTimeString());
+// console.log(dateObj.getFullYear());
+// console.log(dateObj.getMonth());
+// console.log(dateObj.getDate());
+// console.log(dateObj.getDay());
+// console.log(dateObj.getHours());
+// console.log(dateObj.getMinutes());
+// console.log(dateObj.getSeconds());
+// console.log(typeof dateObj);
+
+let myDate = new Date(2023, 0, 23, 5, 3)
+let myDate1 = new Date("01-14-2023")
+console.log(myDate1.toLocaleString());
+console.log(Math.floor(Date.now()/1000));
+
+myDate.toLocaleString('default', {
+    weekday: 'long'
+})
+
+
+// Operations in JavaScript
+// 1. Addition (+)
+// 2. Subtraction (-)
+// 3. Multiplication (*)
+// 4. Division (/)
+// 5. Modulus (%)
+// 6. Exponentiation (**)
+// 7. Increment (++)
+// 8. Decrement (--)
+// 9. Assignment (=)
+// 10. Equality (==)
+// 11. Strict Equality (===)
+// 12. Inequality (!=)
+// 13. Strict Inequality (!==)
+// 14. Greater than (>)
+// 15. Less than (<)
+// 16. Greater than or equal to (>=)
+// 17. Less than or equal to (<=)
+// 18. Logical AND (&&)
+// 19. Logical OR (||)
+// 20. Logical NOT (!)
+// 21. Bitwise AND (&)
+// 22. Bitwise OR (|)
+// 23. Bitwise XOR (^)
+// 24. Bitwise NOT (~)
+// 25. Left shift (<<)
+// 26. Right shift (>>)
+// 27. Unsigned right shift (>>>)
+// 28. Bitwise AND assignment (&=)
+// 29. Bitwise OR assignment (|=)
+// 30. Bitwise XOR assignment (^=)
+// 31. Left shift assignment (<<=)
+// 32. Right shift assignment (>>=)
+// 33. Unsigned right shift assignment (>>>=)
+// 34. Comma (,)
+// 35. Conditional (ternary) operator (?:)
+// 36. delete
---
+++ JavaScript/basic2.js
@@ -0,0 +1,199 @@
+// Arrays
+
+var myArray = new Array();
+myArray[0] = 100;
+myArray[1] = "Adam";
+myArray[2] = true;
+
+var myArray2 = [100, "Adam", true];
+
+const myArr = [1,2,3,4,5,6,7]
+const myArr2 = ['a', 'b', 'c']
+const myArr3 = [1,2,3,'a',4,'r']
+console.log(myArr) // output: (8)[1, 2, 3   
+console.log(myArr2) // output: (3)['a', 'b', 'c']
+console.log(myArr3) // output: (6)[1, 2, 3, 'a', 4, 'r']
+console.log(myArr[0]) // output: 1
+console.log(myArr2[1]) // output: b
+console.log(myArr3[3]) // output: a
+console.log(myArr.length) // output: 7
+console.log(myArr2.length) // output: 3
+console.log(myArr3.length) // output: 6
+console.log(Array.isArray(myArr)) // output: true
+console.log(Array.isArray(myArr2)) // output: true
+console.log(Array.isArray(myArr3)) // output: true
+console.log(myArr.indexOf(5)) // output: 4
+
+// 2. Array Methods
+console.log(myArr.push(8)) // output: 8
+console.log(myArr) // output: (8)[1, 2, 3, 4, 5, 6, 7, 8]
+console.log(myArr.pop()) // output: 8
+console.log(myArr) // output: (7)[1, 2, 3, 4, 5, 6, 7]
+console.log(myArr.unshift(0)) // output: 8
+console.log(myArr) // output: (8)[0, 1, 2, 3, 4, 5, 6, 7]
+console.log(myArr.shift()) // output: 0
+console.log(myArr) // output: (7)[1, 2, 3, 4, 5, 6, 7]
+
+// .join ->
+console.log(myArr.join()) // output: 1,2,3,4,5,6,7
+console.log(myArr.join('')) // output: 1234567
+console.log(myArr.join('-')) // output: 1-2-3-4-5-6-7
+console.log(myArr.join(' ')) // output: 1 2 3 4 5 6 7
+
+// .slice ->
+console.log(myArr.slice(2)) // output: (5)[3, 4, 5, 6, 7]
+console.log(myArr.slice(2, 5)) // output: (3)[3, 4, 5]
+
+// .splice ->
+console.log(myArr.splice(2)) // output: (5)[3, 4, 5, 6, 7]
+console.log(myArr) // output: (2)[1, 2]
+console.log(myArr.splice(2, 0, 'a', 'b')) // output: []
+console.log(myArr) // output: (4)[1, 2, 'a', 'b']
+
+// Difference b/w slice and splice 
+// slice -> it will not change the original array
+// splice -> it will change the original array
+
+// .concat ->
+console.log(myArr.concat(myArr2)) // output: (7)[1, 2, 'a', 'b', 'a', 'b', 'c']
+console.log(myArr.concat(myArr2, myArr3)) // output: (13)[1, 2, 'a', 'b', 'a', 'b', 'c', 1, 2, 3, 'a', 4, 'r']
+
+// .reverse ->
+console.log(myArr.reverse()) // output: (4)['b', 'a', 2, 1]
+
+// .sort ->
+console.log(myArr.sort()) // output: (4)[1, 2, 'a', 'b']
+
+// .fill ->
+console.log(myArr.fill('a')) // output: (4)['a', 'a', 'a', 'a']
+
+// .includes ->
+console.log(myArr.includes('a')) // output: true
+
+// .flat ->
+const myArr4 = [1,2,3,[4,5,6,[7,8,9]]]
+console.log(myArr4.flat()) // output: (9)[1, 2, 3, 4, 5, 6, Array(3)]
+console.log(myArr4.flat(2)) // output: (9)[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(Array.from("Abhay"));
+console.log(Array.from({name: "Abhay"})); // Interesting  Case -> give empty array when conversion nt happen
+console.log(Array.from([1,2,3,4,5], x => x * 2)); // output: (5)[2, 4, 6, 8, 10]
+
+
+// Objects 
+// 1. Object Literal
+const person = {
+    firstName: "Abhay",
+    lastName: "Kumar",
+    age: 21,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: "50 main st",
+        city: "Boston",
+        state: "MA"
+    }
+}
+console.log(person)
+console.log(person.firstName, person.lastName)
+console.log(person["firstName"])
+console.log(person.hobbies[1])
+console.log(person.address.city)
+
+Object.freeze(person);
+person.firstName = "Abhay Gupta"
+console.log(person.firstName) // Not change the value of firstName because of freeze method
+
+
+
+const mySym = Symbol("key1")
+const person2 = {
+    [mySym] : "myKey1", // correct syntax of symbol
+    "full name" : "Abhay Gupta"
+}
+
+console.log(person2["full name"])
+// console.log(person.full name) // Not possible to access like this 
+console.log(person2[mySym]) 
+
+
+
+// 2. Object Constructor
+// const person1 = {}; // non-single term
+const person1 = new Object(); // single term
+person1.firstName = "Abhay";
+person1.lastName = "Kumar";
+person1.age = 21;
+
+console.log(person1)
+console.log(person1.firstName, person1.lastName)
+
+// .objectassign ->
+const person3 = Object.assign({}, person1, person2)
+console.log(person3)
+
+
+// 3. Object Constructor with function
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+}
+let p = new Person("John","Doe",'August 19, 1999');
+console.log(p)
+
+
+// 4. Object Prototype
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+}
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+let p1 = new Person("John","Doe",'August 19, 1999');
+console.log(p1.getBirthYear())
+console.log(p1.getFullName())
+
+// 5. Object.create
+const personPrototypes = {
+    greeting: function() {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
+}
+const mary = Object.create(personPrototypes);
+mary.firstName = "Mary";
+mary.lastName = "Williams";
+mary.age = 30;
+console.log(mary)
+console.log(mary.greeting())
+
+
+// 6. ES6 Classes
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob)
+    }
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+const p2 = new Person("John","Doe",'August 19, 1999');
+console.log(p2.getBirthYear())
+
+
+
+
+
+
+
+
+
