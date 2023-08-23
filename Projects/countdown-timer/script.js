7a38a58ce2ad9907371092bdeba9102a98de69c3
professorabhay <abhayetwup123@gmail.com>
2023-08-23 09:12:23 +0530

    Code

--- Projects/countdown-timer/script.js
+++ Projects/countdown-timer/script.js
@@ -1,11 +1,34 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+
+
+
 const newYears = '1 Jan 2024';
 
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
-    const seconds = (newYearsDate-currentDate) * 1000;
-    const days = Math.floor(seconds / 3600 / 24);
-    console.log(newYearsDate - currentDate)
+    const totalSeconds = (newYearsDate-currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds  % 60);
+    // console.log(days, hours, minutes, seconds)
+
+
+    daysEl.innerHTML = formatTime(days); 
+    hoursEl.innerHTML = formatTime(hours); 
+    minutesEl.innerHTML = formatTime(minutes); 
+    secondsEl.innerHTML = formatTime(seconds); 
+
+}
+
+function formatTime(time){
+    return time<10 ? (`0${time}`) : time;
 }
 
 
