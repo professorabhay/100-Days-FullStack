574e0bbf293e82f525e060ec67c040b8d2afb8e8
professorabhay <abhayetwup123@gmail.com>
2023-09-14 08:28:39 +0530

    Router

---
+++ React.js/07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {userid} = useParams()
+  return (
+    <div>
+      Users: {userid} 
+    </div>
+  )
+}
+
+export default User
