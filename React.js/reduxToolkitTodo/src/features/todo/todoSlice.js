490707e1f11259e48e0ce2b50806359a0bf670b5
professorabhay <abhayetwup123@gmail.com>
2023-09-22 20:20:26 +0530

    Redux Toolkit

---
+++ React.js/reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,19 @@
+import {createSlice, nanoid} from './@reduxjs/tookit'
+
+const initialState = {
+    todos: [{id: 1, text: "Hello World"}]
+}
+
+export const todoSlice = createSlice({
+    name:"todo",
+    initialState ,
+    reducers: {
+        addTodo: (state, action) =>{
+            const todo = { 
+                id: nanoid(),
+                text: action.payload
+            }
+        },
+        removeTodo: ()=>{},
+    }
+})
