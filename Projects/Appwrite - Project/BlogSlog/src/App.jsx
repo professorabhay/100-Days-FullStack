2e11c56481531742a4ede41f1142f4da5841a203
professorabhay <abhayetwup123@gmail.com>
2023-10-01 10:24:17 +0530

    Compoments

--- Projects/Appwrite - Project/BlogSlog/src/App.jsx
+++ Projects/Appwrite - Project/BlogSlog/src/App.jsx
@@ -1,14 +1,38 @@
 import './App.css'
+import {useDispatch} from 'react-redux'
+import React, {useState, useEffect} from 'react'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <div>Blog With Appwrite</div>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-blue-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
